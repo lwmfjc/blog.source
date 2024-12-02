4e29fe7d76c1b3a383b6acaf21f24dd268261466
lwmfjc <lwmfjc@gmail.com>
2024-12-02 08:48:54 +0800

    commit_auto massage

---
+++ book-git.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#"pl"--->"从k40s拉取最新"
+#"ps"--->"推送到k40s"
+cd source/attachments/book
+n=$#
+if [ $n -ne 1 ] ; then
+   echo -e "Please set one params.  \npull-->pull from k40s\npsk40-->push to k40s\n"
+    exit 0
+fi
+x=$1
+#注意下面的文件路径问题
+if [ $x == "pl" ] ; then 
+    echo "pl40--start"
+    #从k40拉取
+    git pull || proxychains git pull
+    echo "pl40--end" 
+elif [ $x == "ps" ] ; then 
+    echo "ps--start"
+    #推送到k40
+    git add .
+    git commit -m "commit_auto massage"
+    git push || proxychains git push
+    echo "ps--end" 
+fi
+exit 1
--- push_auto.sh
+++ push_auto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m "push_auto massage"
+git commit -m "commit_auto massage"
 git push
