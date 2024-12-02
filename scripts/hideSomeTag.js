a03e565b556dc6e9e2209feb0c5c8345951e358b
lwmfjc <lwmfjc@gmail.com>
2024-12-02 12:37:41 +0800

    微调

--- scripts/hideSomeTag.js
+++ scripts/hideSomeTag.js
@@ -19,30 +19,12 @@ hexo.extend.filter.register('before_post_render', function (data) {
      * 忽略iframe链接打开本地pdf的渲染
      *<iframe src='/myjs/pdfjs/web/viewer.html?file=/attachments/hello.pdf' marginwidth ="0" frameborder="no" scrolling="no"  style="padding: 0;width:100%;height: 0px;width: 0px;"></iframe>
      */
-    data.content = data.content.replace(/<iframe.*iframe>/g,
-        function (match_str) {
-            //\1是回溯引用，第一个括号匹配到的内容
-            // \\双斜杠表示匹配\开头，即相对路径
-            if (match_str.search(/src\s*=\s*(['"])[/\\].+\.pdf\s*\1/g) !== -1) {;
-                return "";
-            } else {
-                return match_str;
-            }
-        });
+    data.content = data.content.replace(/<iframe.+src\s*=\s*(['"])[/\\].+\.pdf\s*\1.+iframe>/g,"");
     /**
      * 忽略链接打开本地pdf的渲染
      * <a  href="/myjs/pdfjs/web/viewer.html?file=/attachments/pdf/hello.pdf" target="_blank">第九回 齐侯送文姜婚鲁 祝聃射周王中肩</a>
      */
-    data.content = data.content.replace(/<a.*a>/g,
-        function (match_str) {
-            //\1是回溯引用，第一个括号匹配到的内容
-            // \\双斜杠表示匹配\开头，即相对路径
-            if (match_str.search(/href\s*=\s*(['"])[/\\].+\.pdf\s*\1/g) !== -1) {;
-                return "";
-            } else {
-                return match_str;
-            }
-        });
+    data.content = data.content.replace(/<a.+href\s*=\s*(['"])[/\\].+\.pdf\s*\1.+a>/g,"");
     /**
      * 忽略excalidraw链接
      * %%[xxxEdit in Excalidraw](attachments/img/ecd-20241202113023866.md)%%
