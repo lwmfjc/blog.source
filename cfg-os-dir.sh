dd28113e0d9e53dbd3c2cb7a15185ecf1cae2628
lwmfjc <lwmfjc@gmail.com>
2024-12-03 10:56:49 +0800

    obsidian 配置备份

--- cfg-os-dir.sh
+++ cfg-os-dir.sh
@@ -12,21 +12,23 @@ x=$1
 #注意下面的文件路径问题
 if [ $x == "pcb" ] ; then 
     echo "pcb1"
+    rm -rf source/_os_pc_backup
     #备份pc上的source的obsidian配置
-    cp -r source/.obsidian source/_os_pc_backup
+    cp -a source/.obsidian source/_os_pc_backup
 elif [ $x == "pcr" ] ; then 
     echo "pcr1"
     rm -rf source/.obsidian
     #恢复pc上的source的obsidian配置
-    cp -r source/_os_pc_backup source/.obsidian
+    cp -a source/_os_pc_backup source/.obsidian
 elif [ $x == "mbb" ] ; then 
     echo "mbb1"
+    rm -rf source/_os_mobile_backup
     #备份mobile上的source的obsidian配置
-   cp -r source/.obsidian source/_os_mobile_backup
+   cp -a source/.obsidian source/_os_mobile_backup
 elif [ $x == "mbr" ] ; then 
     echo "mbr1"
     rm -rf source/.obsidian
     #恢复mobile上的source的obsidian配置
-    cp -r source/_os_mobile_backup source/.obsidian
+    cp -a source/_os_mobile_backup source/.obsidian
 fi
 exit 1
